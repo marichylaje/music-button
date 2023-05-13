de8e93d2d1adfc131e8939475dbe6ee16319e00c
Mariano Laje Arrigoni <marianoarrigoni@outlook.com>
2023-05-13 19:22:58 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Searcher from './Searcher.js';
 
 const colorList = [
   '#fdcae1',
@@ -73,9 +74,46 @@ const App = () => {
     }
   };
 
+  const CLIENT_ID=process.env.REACT_APP_SPOTIFY_CLIENT_ID
+  const REDIRECT_URI =process.env.REACT_APP_SPOTIFY_REDIRECT_URI 
+  const AUTH_ENDPOINT =process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT 
+  const RESPONSE_TYPE = process.env.REACT_APP_SPOTIFY_RESPONSE_TYPE
+
+   const [token, setToken] = useState("");
+
+   useEffect(() => { 
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem("token");
+  
+        if (hash && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+            window.location.hash = "";
+            window.localStorage.setItem("token", token);
+        }
+
+        setToken(token)
+    }, [])
+
+    const logout = () => {
+        setToken("");
+        window.localStorage.removeItem("token");
+    }
+
   return (
     <div className="App">
       <div className="top-section" style={{ backgroundColor: bgColor }}>
+      {!token ? 
+              <div > 
+                <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+                 Login to Spotify
+               </a>
+              </div> 
+              :
+              <div> 
+                <Searcher token={token} />
+                <button className= "logOut"onClick={logout}>Logout</button> 
+              </div >
+            }
         {buttons.map((item, index) => (
           <button
             key={index}
